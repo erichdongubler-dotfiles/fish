84ff39e04935738685a6c6aaeeff13d5887016eb
Erich Gubler <erichdongubler@gmail.com>
2017-11-03 13:08:06 -0600

    Rearrange config.fish a bit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,11 @@
-function __erichdongubler_fish_init --description "Initialize @erichdongubler's common shell configuration"
+set fish_greeting
+
+function __check_for_fisherman --description "Initialize @erichdongubler's common shell configuration"
 	set fisherman_location "$HOME/.config/fish/functions/fisher.fish"
 	if not test -e "$fisherman_location"
 		echo "Fisherman not found, downloading..."
 		curl -Lo "$fisherman_location" --create-dirs https://git.io/fisher
+		fisher
 	end
-
-	fisher
 end
-
-__erichdongubler_fish_init
-
-set fish_greeting
+__check_for_fisherman
